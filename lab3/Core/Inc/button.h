03cabe6164280d36e079fb5a9bf5b72cc33dba72
nptakudo <thinh.nguyenph@hcmut.edu.vn>
2023-10-21 13:59:19 +0700

    init ex1

---
+++ lab3/Core/Inc/button.h
@@ -0,0 +1,20 @@
+/*
+ * button.h
+ *
+ *  Created on: Oct 20, 2023
+ *      Author: takudo
+ */
+
+#ifndef INC_BUTTON_H_
+#define INC_BUTTON_H_
+
+#include "main.h"
+
+#define NORMAL_STATE GPIO_PIN_SET
+#define PRESSED_STATE GPIO_PIN_RESET
+
+void getKeyInput();//xuly chong rung + nhan de
+
+
+
+#endif /* INC_BUTTON_H_ */
