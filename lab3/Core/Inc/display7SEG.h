4d78e1f990f42fb6ecaebfc249f3813ad012c705
nptakudo <thinh.nguyenph@hcmut.edu.vn>
2023-10-21 20:20:14 +0700

    ex5

---
+++ lab3/Core/Inc/display7SEG.h
@@ -0,0 +1,17 @@
+/*
+ * display7SEG.h
+ *
+ *  Created on: Oct 21, 2023
+ *      Author: takudo
+ */
+
+#ifndef INC_DISPLAY7SEG_H_
+#define INC_DISPLAY7SEG_H_
+
+#include "global.h"
+
+void clearSignal();
+void display7SEG(int num);
+void displaySignal(int index);
+
+#endif /* INC_DISPLAY7SEG_H_ */
